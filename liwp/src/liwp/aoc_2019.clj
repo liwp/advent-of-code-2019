7b976e968bdf0da44ddbb1944f7135413dfd2a78
Lauri Pesonen <lauri.pesonen@bigpayme.com>
2019-12-10 10:48:47 +0000

    LIWP - clean up the main day runner

--- liwp/src/liwp/aoc_2019.clj
+++ liwp/src/liwp/aoc_2019.clj
@@ -5,29 +5,43 @@
             [liwp.aoc-2019.day03]
             [liwp.aoc-2019.day04]
             [liwp.aoc-2019.day05]
-            [liwp.aoc-2019.day06]))
+            [liwp.aoc-2019.day06]
+            [liwp.aoc-2019.day07]))
 
-(def current-day 6)
+(def day-ns-re #"liwp\.aoc-2019\.day(\d{2})")
 
-(defn parse-args [args]
+(defn day-ns? [ns]
+  (->> ns
+       ns-name
+       str
+       (re-matches day-ns-re)))
+
+(defn all-day-ns []
+  (sort-by str (filter day-ns? (all-ns))))
+
+(defn ns->day [ns]
+  (->> ns
+       ns-name
+       str
+       (re-find day-ns-re)
+       second
+       read-string))
+
+(defn day-ns->fn [ns]
+  (ns-resolve ns (symbol "run")))
+
+(defn args->set [args]
   (->> args
        (map read-string)
        (filter int?)
-       (filter #(<= 1 % current-day))
-       seq))
-
-
-(defn day->fn [day]
-  (let [ns (format "liwp.aoc-2019.day%02d" day)]
-    (resolve (symbol ns "run"))))
+       (into #{})))
 
 (defn -main
   [& args]
   (let [days (if (seq args)
-               (parse-args args)
-               (range 1 (inc current-day)))]
-    (if days
-      (do
-        (println "Executing puzzles for days:" (str/join ", " days))
-        (dorun (map #(time ((day->fn %))) days)))
+               (let [include-day? (args->set args)]
+                 (filter #(-> % ns->day include-day?) (all-day-ns)))
+               (all-day-ns))]
+    (if (seq days)
+      (dorun (map #(time ((day-ns->fn %))) days))
       (println "Invalid arguments:" (str/join " " args)))))
