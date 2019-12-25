819b7a9889ebfbc327538d440de8e08c3e5b4f7e
Michael Williamson <mike@zwobble.org>
2019-12-25 15:45:58 +0000

    Mike: extract State for intcode

--- mwilliamson/aoc/intcode.py
+++ mwilliamson/aoc/intcode.py
@@ -1,28 +1,52 @@
 def run(program, *, next_input=None, output=None):
-    program = list(program)
+    state = State(program)
 
-    position = 0
     while True:
-        opcode = program[position]
-        if opcode == 99:
-            return program
+        opcode = state.opcode()
 
-        if opcode in (1, 2):
-            src_1 = program[program[position + 1]]
-            src_2 = program[program[position + 2]]
+        if opcode == 99:
+            return state.memory
+
+        elif opcode in (1, 2):
+            src_1 = state.read_parameter(0)
+            src_2 = state.read_parameter(1)
 
             if opcode == 1:
                 result = src_1 + src_2
             elif opcode == 2:
                 result = src_1 * src_2
 
-            program[program[position + 3]] = result
-            position += 4
+            state.write_parameter(2, result)
+            state.instruction_pointer += 4
 
         elif opcode == 3:
-            program[program[position + 1]] = next_input()
-            position += 2
+            state.write_parameter(0, next_input())
+            state.instruction_pointer += 2
 
         elif opcode == 4:
-            output(program[program[position + 1]])
-            position += 2
+            output(state.read_parameter(0))
+            state.instruction_pointer += 2
+
+        else:
+            raise ValueError("unknown opcode: {}".format(opcode))
+
+
+class State(object):
+    def __init__(self, memory):
+        self.memory = list(memory)
+        self.instruction_pointer = 0
+
+    def opcode_value(self):
+        return self.memory[self.instruction_pointer]
+
+    def opcode(self):
+        return self.opcode_value() % 100
+
+    def read_parameter(self, index):
+        return self.memory[self._read_immediate_parameter(index)]
+
+    def write_parameter(self, index, value):
+        self.memory[self._read_immediate_parameter(index)] = value
+
+    def _read_immediate_parameter(self, index):
+        return self.memory[self.instruction_pointer + index + 1]
