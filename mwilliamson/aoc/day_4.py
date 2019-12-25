0b86916d0dd15b6bb8bbdfa8178d967674729d8a
Michael Williamson <mike@zwobble.org>
2019-12-25 10:47:58 +0000

    Mike: day four part two

--- mwilliamson/aoc/day_4.py
+++ mwilliamson/aoc/day_4.py
@@ -1,19 +1,60 @@
 def main():
-    assert _is_possible_password("111111")
-    assert not _is_possible_password("223450")
-    assert not _is_possible_password("123789")
+    _part_1()
+    _part_2()
+
+
+def _part_1():
+    print("part 1")
+
+    def is_possible_password(number):
+        return _has_same_adjacent_digits(number) and _digits_never_decrease(number)
+
+    assert is_possible_password("111111")
+    assert not is_possible_password("223450")
+    assert not is_possible_password("123789")
 
     count = 0
 
     for number in range(245182, 790572 + 1):
-        if _is_possible_password(str(number)):
+        if is_possible_password(str(number)):
             count += 1
 
     print(count)
 
 
-def _is_possible_password(number):
-    return _has_same_adjacent_digits(number) and _digits_never_decrease(number)
+def _part_2():
+    print("part 2")
+
+    def is_possible_password(number):
+        return _has_same_adjacent_isolated_digits(number) and _digits_never_decrease(number)
+
+    assert not is_possible_password("111111")
+    assert not is_possible_password("223450")
+    assert not is_possible_password("123789")
+
+
+    assert is_possible_password("112233")
+    assert not is_possible_password("123444")
+    assert is_possible_password("111122")
+
+    count = 0
+
+    for number in range(245182, 790572 + 1):
+        if is_possible_password(str(number)):
+            count += 1
+
+    print(count)
+
+
+def _has_same_adjacent_isolated_digits(number):
+    return any(
+        (
+            number[index] == number[index + 1] and
+            number[index] != _get_or_none(number, index - 1) and
+            number[index] != _get_or_none(number, index + 2)
+        )
+        for index in range(len(number) - 1)
+    )
 
 
 def _has_same_adjacent_digits(number):
@@ -28,6 +69,13 @@ def _digits_never_decrease(number):
         number[index] <= number[index + 1]
         for index in range(len(number) - 1)
     )
+
+
+def _get_or_none(value, index):
+    try:
+        return value[index]
+    except IndexError:
+        return None
 
 
 if __name__ == "__main__":
